121cdb3c169e0679b330efd03164b0392d90f914
Houcheng <houcheng@gmail.com>
2019-04-18 08:04:14 +0800

    merges two commands into one

--- SubGrep.py
+++ SubGrep.py
@@ -3,12 +3,29 @@ import re
 
 class SubGrepCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    for region in self.view.sel():
-      if not region.empty():
+    # If not the same view, run the regular expression
+    if self.view != sublime.active_window().active_sheet().view():
+        search_text = self.view.substr(sublime.Region(0, self.view.size()))
+        title = "RE [%s]" % search_text
+        syntax = self.view.settings().get('syntax')
+        text = self.get_text_from_active_sheet()
+        self.view.run_command( 'show_reg', {'title': title, 'search_text': search_text, 'syntax': syntax, 'text': text} )
+
+    # If selection is not empty, run the grep
+    elif not self.view.sel()[0].empty():
+        region = self.view.sel()[0]
         selection = self.view.substr(region)
         title = "Grep: [%s]" % selection
         syntax = self.view.settings().get('syntax')
         self.view.run_command( 'show_grep', {'title': title, 'selection': selection, 'syntax': syntax} )
+
+  # get text from active view
+  def get_text_from_active_sheet(self):
+    active_view = sublime.active_window().active_sheet().view()
+    region = sublime.Region(0, active_view.size())
+    text = active_view.substr(region) 
+    return text   
+
 class ShowGrepCommand(sublime_plugin.TextCommand):
   def run(self,edit,title,selection,syntax):
     self.grep_view(edit,title,selection, syntax)
@@ -29,23 +46,6 @@ class ShowGrepCommand(sublime_plugin.TextCommand):
 
     return panel
 
-# grep while in find view
-class SubRegCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
-    search_text = self.view.substr(sublime.Region(0, self.view.size()))
-    if self.view != sublime.active_window().active_sheet().view():
-        title = "RE [%s]" % search_text
-        syntax = self.view.settings().get('syntax')
-        text = self.get_text_from_active_sheet()
-        self.view.run_command( 'show_reg', {'title': title, 'search_text': search_text, 'syntax': syntax, 'text': text} )
-
-  # get text from active view
-  def get_text_from_active_sheet(self):
-    active_view = sublime.active_window().active_sheet().view()
-    region = sublime.Region(0, active_view.size())
-    text = active_view.substr(region) 
-    return text   
-
 class ShowRegCommand(sublime_plugin.TextCommand):
   def run(self,edit,title,search_text,syntax,text):
     self.grep_view(edit,title,search_text,syntax,text)
@@ -56,7 +56,7 @@ class ShowRegCommand(sublime_plugin.TextCommand):
     panel.set_scratch(True)
     panel.set_syntax_file(syntax)
     # do regular express here
-    pattern = re.compile(search_text)
+    pattern = re.compile(search_text, re.I)
     result = ""
     for line in text.split('\n'):
       if pattern.search(line):
